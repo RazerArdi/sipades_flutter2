0d6a550a7bf6c0dc7a2346a80c6d477fa1b6bb21
bayua <bayuardiyansyah30@gmail.com>
2024-09-29 19:00:41 +0700

    JadwalSholatScreen, cuaca, event desa pra-coded

--- lib/FiturTambahan/EventDesaScreen.dart
+++ lib/FiturTambahan/EventDesaScreen.dart
@@ -1,55 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart'; // For LatLng coordinates
 
-class EventDesaScreen extends StatelessWidget {
+class EventDesaScreen extends StatefulWidget {
+  @override
+  _EventDesaScreenState createState() => _EventDesaScreenState();
+}
+
+class _EventDesaScreenState extends State<EventDesaScreen> {
+  final MapController _mapController = MapController();
+  final LatLng _center = LatLng(-6.24064226674634, 106.84254620231945); // Jakarta
+  final List<Marker> _markers = [];
+
+  // Function to add markers to the list of markers
+  void _addMarker(LatLng position) {
+    setState(() {
+      _markers.add(
+        Marker(
+          width: 80.0,
+          height: 80.0,
+          point: position,
+          child: Builder(
+          builder: (ctx) => Icon(
+            Icons.location_on,
+            color: Colors.red,
+            size: 40.0,
+          ),
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Event Desa"),
+        title: Text("Event Desa with Flutter Map"),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromARGB(255, 71, 126, 255),
-              Color.fromARGB(255, 18, 64, 189),
-            ],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: _center,
+              maxZoom: 13.0,
+              onTap: (tapPosition, point) {
+                _addMarker(point);
+              },
+            ),
             children: [
-              Icon(
-                Icons.construction,
-                size: 100,
-                color: Colors.white,
+              TileLayer(
+                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+                userAgentPackageName: 'com.example.eventdesa',
               ),
-              SizedBox(height: 20),
-              Text(
-                'Halaman Ini Masih Sedang Perbaikan!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Mohon maaf atas ketidaknyamanan ini. Kami sedang melakukan perbaikan.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
+              MarkerLayer(
+                markers: _markers,
               ),
             ],
           ),
-        ),
+          Positioned(
+            top: 10,
+            left: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Search for event...',
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            left: 10,
+            child: FloatingActionButton(
+              onPressed: () {
+                _addMarker(_center);
+              },
+              tooltip: 'Add Marker',
+              child: Icon(Icons.add_location),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/FiturTambahan/JadwalSholatScreen.dart
+++ lib/FiturTambahan/JadwalSholatScreen.dart
@@ -5,51 +5,128 @@ class JadwalSholatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Jadwal Sholat"),
-        backgroundColor: Colors.blue,
+        title: Text("Waktu Sholat"),
+        backgroundColor: Colors.purple,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          )
+        ],
       ),
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromARGB(255, 71, 126, 255),
-              Color.fromARGB(255, 18, 64, 189),
-            ],
-          ),
+          color: Colors.white70, // Background color for the whole body
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          children: <Widget>[
+            // Background image at the top
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height, // Set height to 40% of the screen height
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/masjid_background.png"),
+                    fit: BoxFit.cover, // Use BoxFit.cover to maintain aspect ratio
+                  ),
+                ),
+              ),
+            ),
+            // Content below the image
+            Column(
+              children: <Widget>[
+                SizedBox(height: 25), // Space for the image
+                // Bagian atas dengan waktu shalat berikutnya
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.nightlight_round,
+                        color: Colors.blue,
+                        size: 80.0,
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        "Isya 07:06",
+                        style: TextStyle(
+                          fontSize: 48.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(
+                        "2 Jam 25 Menit Lagi",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Bagian bawah dengan daftar waktu shalat
+                Container(
+                  color: Colors.white70,
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Hari ini",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text("Jumat, 30 Juni 2017 / 6 Syawal 1438"),
+                      SizedBox(height: 16.0),
+                      _buildWaktuShalatItem("Subuh", "04:43 AM", true),
+                      _buildWaktuShalatItem("Fajar", "06:04 AM", false),
+                      _buildWaktuShalatItem("Dzhur", "11:59 AM", true),
+                      _buildWaktuShalatItem("Ashar", "03:20 PM", true),
+                      _buildWaktuShalatItem("Maghrib", "05:52 PM", true),
+                      _buildWaktuShalatItem("Isya", "07:06 PM", true),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWaktuShalatItem(String waktu, String jam, bool isActive) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            waktu,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Row(
             children: [
+              Text(
+                jam,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: isActive ? FontWeight.bold : FontWeight.normal,
+                ),
+              ),
+              SizedBox(width: 8.0),
               Icon(
-                Icons.construction,
-                size: 100,
-                color: Colors.white,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Halaman Ini Masih Sedang Perbaikan!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Mohon maaf atas ketidaknyamanan ini. Kami sedang melakukan perbaikan.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
+                isActive ? Icons.notifications_active : Icons.notifications_off,
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
